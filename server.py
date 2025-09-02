cd5cc9a55b97b6bb4a6f54da038268ba21891b50
Nicolas Lallier <nicolas.lallier@famillelallier.net>
2025-09-02 16:57:49 -0400

    new code

--- server.py
+++ server.py
@@ -155,4 +155,10 @@ app = Starlette(
 # For local dev: uvicorn server:app --port 8000 --host 0.0.0.0
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=DEFAULT_PORT)
+    uvicorn.run(
+    "server:app",
+    host="0.0.0.0",
+    port=int(os.getenv("PORT", "8000")),
+    proxy_headers=True,
+    forwarded_allow_ips="*",
+)
